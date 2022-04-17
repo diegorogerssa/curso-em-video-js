ba45c0a5cda6e9ca19db0aa1a2ff47dc13bacf65
Rogers <rogersmhdg@gmail.com>
2022-04-17 10:53:21 -0300

    Aula-12

---
+++ ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var horas = agora.getHours()
+console.log(`Agora são ${horas} horas`)
+
+if (horas <6) {
+    console.log('Boa madrugada!')
+}else if(horas <12){
+    console.log('Bom dia!')
+}else if (horas <18){
+    console.log('Boa tarde!')
+}else{
+    console.log('Boa noite!')
+}
