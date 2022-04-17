83bdc3c027db8cf1fdedbf5c27a8e5cc12e1dca0
Rogers <rogersmhdg@gmail.com>
2022-04-17 15:36:35 -0300

    continuar após ver funções

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificar(){
+var datual = new Date()
+var data = datual.getFullYear()
+var formAno = window.document.getElementById ('txtano')
+var res = window.document.querySelector('div#res')
+ 
+   
+ }
